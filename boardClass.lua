202e47526ed7dd8f46d174602bc7fdc55f4fea7a
Matthew Leiman <dev@leiman.com>
2022-05-06 06:15:08 +0000

    removed sb class to fix memory issue. smaller json

--- boardClass.lua
+++ boardClass.lua
@@ -1,14 +1,21 @@
 import "CoreLibs/object"
+import "CoreLibs/graphics"
 import "CoreLibs/sprites"
-import "smallBoxClass"
+
+-- import "smallBoxClass"
+
+local gfx <const> = playdate.graphics
+
+status = {
+    ["Given"] = 1,
+    ["Empty"] = 2,
+    ["Guessed"] = 3
+}
 
 
 
+class('Board').extends(gfx.sprite)
 
-
-
-class('Board').extends()
--- 
 function Board:init(template)
     self.boxs = {}
     self.rows = {{}, {}, {}, {}, {}, {}, {}, {}, {}}
@@ -17,7 +24,13 @@ function Board:init(template)
     for r=1, 9 do
         for c=1, 9 do
             n = (c) + ((r-1) * 9)
-            local b = smallBox(r,c,template[n] ~= '.' and tonumber(template[n]) or 0, self)
+            local b = {
+                ["row"]=r,
+                ["column"]=c,
+                ["number"]=template[n] ~= "." and template[n] or 0,
+                ["status"]=template[n]  ~= "." and status["Given"] or status["Empty"],
+                ["bigBox"]=findBigBoxGivenRowAndColumn(r,c)
+            }
             table.insert(self.boxs,b)
             table.insert(self.rows[b.row],b)
             table.insert(self.columns[b.column],b)
@@ -25,6 +38,160 @@ function Board:init(template)
         end
     end
     self.selected = self.boxs[1]
+end
+
+
+
+
+
+function Board:draw(x, y, width, height)
+    local thickLineWidth, thinLineWidth = 5, 1
+    local smallBoxWidth, smallBoxHeight = width/9, height/9
+    
+    gfx.setLineWidth(thickLineWidth)
+    -- gfx.setColor(gfx.kColorBlack)
+    gfx.drawRect(0, 0, width, height)
+    -- Draw Thick Lines
+    gfx.drawLine(0,height/3,width,height/3)
+    gfx.drawLine(0,2*height/3,width,2*height/3)
+    gfx.drawLine(width/3,0,width/3,height)
+    gfx.drawLine(2*width/3,0,2*width/3,height)
+    -- Draw Thin Lines
+    gfx.setLineWidth(thinLineWidth)
+    gfx.drawLine(0,(1*height)/9,width,(1*height)/9)
+    gfx.drawLine(0,(2*height)/9,width,(2*height)/9)
+    gfx.drawLine(0,(4*height)/9,width,(4*height)/9)
+    gfx.drawLine(0,(5*height)/9,width,(5*height)/9)
+    gfx.drawLine(0,(7*height)/9,width,(7*height)/9)
+    gfx.drawLine(0,(8*height)/9,width,(8*height)/9)
+    gfx.drawLine((1*width)/9,0,(1*width)/9,height)
+    gfx.drawLine((2*width)/9,0,(2*width)/9,height)
+    gfx.drawLine((4*width)/9,0,(4*width)/9,height)
+    gfx.drawLine((5*width)/9,0,(5*width)/9,height)
+    gfx.drawLine((7*width)/9,0,(7*width)/9,height)
+    gfx.drawLine((8*width)/9,0,(8*width)/9,height)
+    gfx.setColor(gfx.kColorBlack)
+    
+    local selectedx, selectedy = ((self.selected.column-1)*smallBoxWidth), ((self.selected.row-1)*smallBoxHeight)
+    playdate.graphics.fillRect(selectedx, selectedy, smallBoxWidth+1, smallBoxHeight+1)
+    for index=1, 81 do
+        local sbx = ((self.boxs[index].column-1)*smallBoxWidth)
+        local sby = ((self.boxs[index].row-1)*smallBoxHeight)
+        local text = self.boxs[index].number ~= 0 and self.boxs[index].number or ''
+        if self.boxs[index].status == status["Given"] then
+            local textWidth = gfx.getTextSize("*"..text)
+            local textHeight = gfx.getFont(gfx.font.kVariantBold):getHeight()
+            gfx.drawText("*"..text, sbx + (smallBoxWidth/2 - textWidth/2), sby+(smallBoxHeight - textHeight)) 
+        else
+            local textWidth = gfx.getTextSize(text)
+            local textHeight = gfx.getFont():getHeight()
+            gfx.drawText(text,sbx+ (smallBoxWidth/2 - textWidth/2),sby+(smallBoxHeight - textHeight))
+            -- gfx.drawText(text, sbx + (smallBoxWidth/2 - textWidth/2), sby-(smallBoxHeight + textHeight))
+        end
+    end
+end
+
+
+
+function generateBoard(template)
+    local board = {}
+    board.boxs = {}
+    board.rows = {{}, {}, {}, {}, {}, {}, {}, {}, {}}
+    board.columns = {{}, {}, {}, {}, {}, {}, {}, {}, {}}
+    board.bigBoxs = {{}, {}, {}, {}, {}, {}, {}, {}, {}}
+    for r=1, 9 do
+        for c=1, 9 do
+            n = (c) + ((r-1) * 9)
+            -- local b = smallBox(r,c,template[n] ~= '.' and tonumber(template[n]) or 0, self)
+            local b = {
+                ["row"]=r,
+                ["column"]=c,
+                ["number"]=template[n] ~= "." and template[n] or 0,
+                ["status"]=template[n] ~= "." and status["Given"] or status["Empty"],
+                ["bigBox"]=findBigBoxGivenRowAndColumn(r,c)
+            }
+            if board.number == 0 then
+                board.status = status['Empty']
+            end
+            table.insert(board.boxs,b)
+            table.insert(board.rows[b.row],b)
+            table.insert(board.columns[b.column],b)
+            table.insert(board.bigBoxs[b.bigBox],b)
+        end
+    end
+    board.selected = board.boxs[1]
+    return board
+end
+
+function setDrawForBoardSprite(boardSprite)
+    function boardSprite:draw(x, y, width, height)
+        local thickLineWidth, thinLineWidth = 5, 1
+        local smallBoxWidth, smallBoxHeight = width/9, height/9
+        gfx.setLineWidth(thickLineWidth)
+        -- gfx.setColor(gfx.kColorBlack)
+        gfx.drawRect(0, 0, width, height)
+        -- Draw Thick Lines
+        gfx.drawLine(0,height/3,width,height/3)
+        gfx.drawLine(0,2*height/3,width,2*height/3)
+        gfx.drawLine(width/3,0,width/3,height)
+        gfx.drawLine(2*width/3,0,2*width/3,height)
+        -- Draw Thin Lines
+        gfx.setLineWidth(thinLineWidth)
+        gfx.drawLine(0,(1*height)/9,width,(1*height)/9)
+        gfx.drawLine(0,(2*height)/9,width,(2*height)/9)
+        gfx.drawLine(0,(4*height)/9,width,(4*height)/9)
+        gfx.drawLine(0,(5*height)/9,width,(5*height)/9)
+        gfx.drawLine(0,(7*height)/9,width,(7*height)/9)
+        gfx.drawLine(0,(8*height)/9,width,(8*height)/9)
+        gfx.drawLine((1*width)/9,0,(1*width)/9,height)
+        gfx.drawLine((2*width)/9,0,(2*width)/9,height)
+        gfx.drawLine((4*width)/9,0,(4*width)/9,height)
+        gfx.drawLine((5*width)/9,0,(5*width)/9,height)
+        gfx.drawLine((7*width)/9,0,(7*width)/9,height)
+        gfx.drawLine((8*width)/9,0,(8*width)/9,height)
+        gfx.setColor(gfx.kColorBlack)
+        local selectedx, selectedy = ((self.boardData.selected.column-1)*smallBoxWidth), ((self.boardData.selected.row-1)*smallBoxHeight)
+        playdate.graphics.fillRect(selectedx, selectedy, smallBoxWidth+1, smallBoxHeight+1)
+        for index=1, 81 do
+            local sbx, sby = ((self.boardData.boxs[index].column-1)*smallBoxWidth), ((self.boardData.boxs[index].row-1)*smallBoxHeight)
+            local text = self.boardData.boxs[index].number ~= 0 and self.boardData.boxs[index].number or ''
+            if self.boardData.boxs[index].status == status["Given"] then
+                local textWidth = gfx.getTextSize("*"..text)
+                local textHeight = gfx.getFont(gfx.font.kVariantBold):getHeight()
+                gfx.drawText("*"..text, sbx + (smallBoxWidth/2 - textWidth/2), sby+(smallBoxHeight - textHeight)) 
+            else
+                local textWidth = gfx.getTextSize(text)
+                local textHeight = gfx.getFont():getHeight()
+                gfx.drawText(text,sbx+ (smallBoxWidth/2 - textWidth/2),sby+(smallBoxHeight - textHeight))
+                -- gfx.drawText(text, sbx + (smallBoxWidth/2 - textWidth/2), sby-(smallBoxHeight + textHeight))
+            end
+        end
+    end
+    return boardSprite
+end
+
+
+
+function findBigBoxGivenRowAndColumn(row,column)
+    if row < 4 and column < 4 then
+        return 1
+    elseif row < 4 and column < 7 then
+        return 2
+    elseif row < 4 then
+        return 3
+    elseif row < 7 and column < 4 then
+        return 4
+    elseif row < 7 and column < 7 then
+        return 5
+    elseif row < 7 then
+        return 6
+    elseif column < 4 then
+        return 7
+    elseif column < 7 then
+        return 8
+    else
+        return 9
+    end
 end
 
 -- function Board:printboard()
--- main.lua
+++ main.lua
@@ -1,47 +1,51 @@
-
-
-import "CoreLibs/object"
+-- import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 
 
-import "smallBoxClass"
+-- import "smallBoxClass"
 import "boardClass"
 
 local gfx <const> = playdate.graphics
 
+gfx.setImageDrawMode(gfx.kDrawModeNXOR)
+gfx.clear()
 
 getmetatable('').__index = function(str,i) return string.sub(str,i,i) end
 
+difficutly = {
+    [1] = "simple",
+    [2] = "easy",
+    [3] = "intermediate",
+    [4] = "expert"
+}
 
 -- local simple_template = '.34....7.......36.71.....28..25.7...57.....1...38..5..82..6........7.14.6..1....3'
-local simple_file = playdate.file.open("puzzles/simple.json")
-local simple_json = json.decodeFile(simple_file)
 math.randomseed(playdate.getSecondsSinceEpoch())
-local _, puzzleCount = table.getsize(simple_json)
-local puzzle_index = math.random(0,puzzleCount)
+local file_number = math.random(1,10)
+local random_difficutly = math.random(1,4)
+local file_name = "puzzles/"..difficutly[random_difficutly]..file_number..".json"
+local file_name = "puzzles/"..difficutly[1]..file_number..".json"
+local simple_file = playdate.file.open(file_name)
+local simple_json = json.decodeFile(simple_file)
+local puzzle_index = math.random(1,10000)
 simple_template = simple_json[""..puzzle_index..""]
 puzzleCount = nil
 simple_file = nil
 simple_json = nil
 
 local mainBoard = Board(simple_template)
+
 -- mainBoard:printboard()
 
 
 function myGameSetUp()
-    local offset = 25
     local midpointx, midpointy = playdate.display.getWidth() / 2, playdate.display.getHeight() / 2
-    local xoff, yoff = midpointx - (5 * offset), midpointy - (5 * offset)
-    for i=1,#mainBoard.boxs do
-        local x, y = xoff + (mainBoard.boxs[i].column * offset), yoff + (mainBoard.boxs[i].row * offset)
-        mainBoard.boxs[i]:moveTo( x, y )
-        mainBoard.boxs[i]:add()
-        mainBoard.boxs[i]:setSize(offset,offset)
-    end
-    
-
+    local boardSize = playdate.display.getHeight() *.95
+    mainBoard:add()
+    mainBoard:setSize(boardSize,boardSize)
+    mainBoard:moveTo(midpointx,midpointy)
 end
 
 myGameSetUp()
@@ -73,78 +77,83 @@ end
 
 local movespeed = 200
 
+
+
+
+
+
+
+
+
+
 function playdate.update()
 
-
-    if playdate.buttonIsPressed( playdate.kButtonUp ) then
-        if upCanBePressed then
-            upCanBePressed = false
-            playdate.timer.new(movespeed,setUpCanBePressedToTrue)
-            if mainBoard.selected.row ~= 1 then
-                mainBoard.selected:markDirty()
-                mainBoard.selected = mainBoard.rows[mainBoard.selected.row -1][mainBoard.selected.column]
-                mainBoard.selected:markDirty()
+if playdate.buttonIsPressed( playdate.kButtonUp ) then
+    if upCanBePressed then
+        upCanBePressed = false
+        playdate.timer.new(movespeed,setUpCanBePressedToTrue)
+        if mainBoard.selected.row ~= 1 then
+            mainBoard.selected = mainBoard.rows[mainBoard.selected.row -1][mainBoard.selected.column]
+            mainBoard:markDirty()
+        end
+    end
+end
+if playdate.buttonIsPressed( playdate.kButtonRight ) then
+    if rightCanBePressed then
+        rightCanBePressed = false
+        playdate.timer.new(movespeed,setRightCanBePressedToTrue)
+        if mainBoard.selected.column ~= 9 then
+            mainBoard.selected = mainBoard.columns[mainBoard.selected.column +1][mainBoard.selected.row]
+            mainBoard:markDirty()
+        end
+    end
+end
+if playdate.buttonIsPressed( playdate.kButtonDown ) then
+    if downCanBePressed then
+        downCanBePressed = false
+        playdate.timer.new(movespeed,setDownCanBePressedToTrue)
+        if mainBoard.selected.row ~= 9 then
+            mainBoard.selected = mainBoard.rows[mainBoard.selected.row +1][mainBoard.selected.column]
+            mainBoard:markDirty()
+        end
+    end
+end
+if playdate.buttonIsPressed( playdate.kButtonLeft ) then
+    if leftCanBePressed then
+        leftCanBePressed = false
+        playdate.timer.new(movespeed,setLeftCanBePressedToTrue)
+        if mainBoard.selected.column ~= 1 then
+            mainBoard.selected = mainBoard.columns[mainBoard.selected.column - 1][mainBoard.selected.row]
+            mainBoard:markDirty()
+        end
+    end
+end
+if playdate.buttonIsPressed( playdate.kButtonA ) then
+    if ACanBePressed then
+        ACanBePressed = false
+        playdate.timer.new(movespeed,setACanBePressedToTrue)
+        if mainBoard.selected.status == status["Empty"] or mainBoard.selected.status == status["Guessed"] then
+            
+            if mainBoard.selected.number ~= 9 then
+                mainBoard.selected.number = mainBoard.selected.number + 1
+                mainBoard:markDirty()
             end
         end
     end
-    if playdate.buttonIsPressed( playdate.kButtonRight ) then
-        if rightCanBePressed then
-            rightCanBePressed = false
-            playdate.timer.new(movespeed,setRightCanBePressedToTrue)
-            if mainBoard.selected.column ~= 9 then
-                mainBoard.selected:markDirty()
-                mainBoard.selected = mainBoard.columns[mainBoard.selected.column +1][mainBoard.selected.row]
-                mainBoard.selected:markDirty()
+end
+if playdate.buttonIsPressed( playdate.kButtonB ) then
+    if BCanBePressed then
+        BCanBePressed = false
+        playdate.timer.new(movespeed,setBCanBePressedToTrue)
+        if mainBoard.selected.status == status["Empty"] or mainBoard.selected.status == status["Guessed"] then
+            if mainBoard.selected.number ~= 0 then
+                mainBoard:markDirty()
+                mainBoard.selected.number = mainBoard.selected.number - 1
             end
         end
     end
-    if playdate.buttonIsPressed( playdate.kButtonDown ) then
-        if downCanBePressed then
-            downCanBePressed = false
-            playdate.timer.new(movespeed,setDownCanBePressedToTrue)
-            if mainBoard.selected.row ~= 9 then
-                mainBoard.selected:markDirty()
-                mainBoard.selected = mainBoard.rows[mainBoard.selected.row +1][mainBoard.selected.column]
-                mainBoard.selected:markDirty()
-            end
-        end
-    end
-    if playdate.buttonIsPressed( playdate.kButtonLeft ) then
-        if leftCanBePressed then
-            leftCanBePressed = false
-            playdate.timer.new(movespeed,setLeftCanBePressedToTrue)
-            if mainBoard.selected.column ~= 1 then
-                mainBoard.selected:markDirty()
-                mainBoard.selected = mainBoard.columns[mainBoard.selected.column - 1][mainBoard.selected.row]
-                mainBoard.selected:markDirty()
-            end
-        end
-    end
-    if playdate.buttonIsPressed( playdate.kButtonA ) then
-        if ACanBePressed then
-            ACanBePressed = false
-            playdate.timer.new(movespeed,setACanBePressedToTrue)
-            if mainBoard.selected.status == status["Empty"] or mainBoard.selected.status == status["Guessed"] then
-                if mainBoard.selected.number ~= 9 then
-                    mainBoard.selected:markDirty()
-                    mainBoard.selected.number = mainBoard.selected.number + 1
-                end
-            end
-        end
-    end
-    if playdate.buttonIsPressed( playdate.kButtonB ) then
-        if BCanBePressed then
-            BCanBePressed = false
-            playdate.timer.new(movespeed,setBCanBePressedToTrue)
-            if mainBoard.selected.status == status["Empty"] or mainBoard.selected.status == status["Guessed"] then
-                if mainBoard.selected.number ~= 0 then
-                    mainBoard.selected:markDirty()
-                    mainBoard.selected.number = mainBoard.selected.number - 1
-                end
-            end
-        end
-    end
-
+end
+    
     
     gfx.sprite.update()
     playdate.timer.updateTimers()
